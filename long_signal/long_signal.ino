668fa8524e922743242f8b312c2194467588bdb6
max <max@max-x41.elec.ac.nz>
2015-05-20 08:43:26 +1200

    code harvesting

--- long_signal/long_signal.ino
+++ long_signal/long_signal.ino
@@ -34,7 +34,7 @@ volatile  unsigned int irBuffer[maxLen]; //stores timings - volatile because cha
 volatile unsigned int x = 0; //Pointer thru irBuffer - volatile because changed by ISR
 
 void setup() {
-  Serial.begin(115200); //change BAUD rate as required
+  Serial.begin(9600); //change BAUD rate as required
   attachInterrupt(0, rxIR_Interrupt_Handler, CHANGE);//set up ISR for receiving IR signal
 }
 
