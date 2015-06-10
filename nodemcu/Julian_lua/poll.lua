c8f037e30811ea8e5731d849d541302c4811533a
marcus <best.nose@gmail.com>
2015-06-10 14:08:40 +1200

    Julian's loginc implemented and pins changed

--- nodemcu/Julian_lua/poll.lua
+++ nodemcu/Julian_lua/poll.lua
@@ -16,8 +16,8 @@
 	OFF	= "0"
 	-- Set state for start up (off): A sensible state
 	powerstate = OFF
-	PINON	= 1	-- set pin to pullup to send on signal
-	PINOFF	= 2	-- set pin to pullup to send off signal
+	PINON	= 6	-- set pin to pullup to send on signal
+	PINOFF	= 5	-- set pin to pullup to send off signal
 	
 -- Setup hardware
 	-- set pins to output
@@ -29,14 +29,12 @@
 -- A simple http client
 	conn=net.createConnection(net.TCP, 0) 
 	conn:on("receive", function(conn, pl)
-		if string.find(pl,"online") then
-			if string.find(pl,"state=0") then gpio.write(PINOFF,gpio.HIGH) gpio.write(PINON,gpio.LOW) print("Turning off...") end
-   			if string.find(pl,"state=1") then gpio.write(PINON,gpio.HIGH) gpio.write(PINOFF,gpio.LOW) print("Turning on..") end
-			end
-		if string.find(pl,"offline") then gpio.write(PINON, gpio.LOW) gpio.write(PINOFF, gpio.LOW) end	
+		if string.find(pl,"state=0") then gpio.write(PINOFF,gpio.HIGH) gpio.write(PINON,gpio.LOW) print("Turning off...") end
+   		if string.find(pl,"state=1") then gpio.write(PINON,gpio.HIGH) gpio.write(PINOFF,gpio.LOW) print("Turning on..") end
+		if string.find(pl,"null") then gpio.write(PINON, gpio.LOW) gpio.write(PINOFF, gpio.LOW) end	
 		end ) -- function ends
-	conn:connect(8000,"192.168.0.23")
-	conn:send("GET / HTTP/1.1\r\nHost: 192.168.0.23\r\n"
+	conn:connect(8000,"192.168.0.9")
+	conn:send("GET / HTTP/1.1\r\nHost: 192.168.0.9\r\n"
 	    .."Connection: keep-alive\r\nAccept: */*\r\n\r\n") -- The .. is the string concatenate string operator. 
 
 
