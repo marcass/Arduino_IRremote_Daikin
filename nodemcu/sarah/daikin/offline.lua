5c62a03d2798b069d1f185d93bba86655c5e1e90
Marcus <best.nose@gmail.com>
2015-08-07 17:19:16 +1200

    no longer needed

--- nodemcu/sarah/daikin/offline.lua
+++
@@ -1,9 +0,0 @@
---obseleted this file
-local broker = "192.168.0.3"
-   print ("Mqtt Reconnecting...")   
-   tmr.alarm(1, 1000, 0, function()  
-     m:connect(broker, 1883, 0, function(conn)   
-        m:subscribe("home/heat/dai/com",0, function(conn)
-      end)
-      end)  
-    end) 
